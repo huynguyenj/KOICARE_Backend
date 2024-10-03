f7cfcdb60a914e0114a7877ad724af158c7112b3
thanhan139 <duongthanhan139@gmail.com>
2024-10-04 01:39:23 +0700

    KoiFish

---
+++ kcs-backend/src/main/java/group7/se1876/kcs_backend/enums/KoiOrigin.java
@@ -0,0 +1,8 @@
+package group7.se1876.kcs_backend.enums;
+
+public enum KoiOrigin {
+    JAPAN,
+    CHINA,
+    VIETNAM,
+    INDONESIA
+}
---
+++ kcs-backend/src/main/java/group7/se1876/kcs_backend/enums/KoiSpecies.java
@@ -0,0 +1,11 @@
+package group7.se1876.kcs_backend.enums;
+
+public enum KoiSpecies {
+    KUMONRYU,
+    GOROMO,
+    TANCHO,
+    SHUSUI,
+    ASAGI,
+    BEKKO,
+    KOHAKU
+}
