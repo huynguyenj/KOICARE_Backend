1e46f7eb7e88e833a549d58e7a10fdec24e4e7c5
thanhan139 <duongthanhan139@gmail.com>
2024-10-07 23:17:38 +0700

    update code

---
+++ kcs-backend/src/main/java/group7/se1876/kcs_backend/exception/ProductAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package group7.se1876.kcs_backend.exception;
+
+public class ProductAlreadyExistsException extends RuntimeException {
+    public ProductAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    
+}
