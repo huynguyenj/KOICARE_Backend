49ca62da9e6a39b67135f3afa272c824a34766e2
huynguyenj <nguyenhuyjobs@gmail.com>
2024-10-08 17:20:14 +0700

    update

--- kcs-backend/src/main/java/group7/se1876/kcs_backend/repository/KoiFishRepository.java
+++
@@ -1,7 +0,0 @@
-package group7.se1876.kcs_backend.repository;
-
-import group7.se1876.kcs_backend.entity.KoiFish;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KoiFishRepository extends JpaRepository<KoiFish, Integer> {
-}
